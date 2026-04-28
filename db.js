7a677b4449196ac6c5f48940509bc9dc2d9495bc
Jul-svoboda <july.svoboda@gmail.com>
2026-04-28 19:12:52 +0300

    feat: connect a remote database

--- db.js
+++ db.js
@@ -1,12 +1,24 @@
 const {Sequelize} = require('sequelize')
 
-module.exports = new Sequelize(
-    process.env.DB_NAME, // название БД
-    process.env.DB_USER, // пользователь
-    process.env.DB_PASSWORD, // пароль
-    {
-        dialect: 'postgres', //передача СУБД
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT
-    } 
-)
+// module.exports = new Sequelize(
+//     process.env.DB_NAME, // название БД
+//     process.env.DB_USER, // пользователь
+//     process.env.DB_PASSWORD, // пароль
+//     {
+//         dialect: 'postgres', //передача СУБД
+//         host: process.env.DB_HOST,
+//         port: process.env.DB_PORT
+//     } 
+// )
+
+module.exports = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
+});
